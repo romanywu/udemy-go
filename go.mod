2a54e791c388f12c6447602402d08603f3927843
Roman Wu <me@romanwu.com>
2025-04-12 20:36:47 -0400

    Updates

---
+++ go.mod
@@ -0,0 +1,3 @@
+module example.com/udemy
+
+go 1.24.1
--- test.go
+++ test.go
@@ -3,5 +3,23 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World")
+	numbers := []int{1, 2, 3}
+	fmt.Println(numbers)
+
+}
+
+func primary(numbers *[]int, transform func(any) int) []int {
+	dNumbers := []int{}
+
+	for _, val := range *numbers {
+		dNumbers = append(dNumbers, transform(val))
+	}
+
+	return dNumbers
+}
+
+func secondary(multiplier int) func(int) int {
+	return func(i int) int {
+		return multiplier * i
+	}
 }
